8379f0794e8040db9841cfe57c5e03a1054edd3e
Mostafa Koolabadi <draxsis.1995@gmail.com>
2025-06-10 14:38:17 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,14 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    base: '/ShiftTracker-Application/', 
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
